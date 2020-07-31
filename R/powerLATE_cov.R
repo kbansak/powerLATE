2c037dcfa428d92f8812823183fdd4b6109ad122
Eddie Yang <43977631+EddieYang211@users.noreply.github.com>
2020-07-30 20:58:23 -0700

    Update documentation and function examples

--- R/powerLATE_cov.R
+++ R/powerLATE_cov.R
@@ -10,11 +10,11 @@
 #' @param pZ            	probability of being assigned to treatment. Default is 0.5, i.e. equal assignment probability.
 #' @param pi            	compliance rate. Equivalently, average causal effect of treatment assignment on treatment uptake.
 #' @param N             	total sample size.
-#' @param kappa         	effect size.
+#' @param kappa         	LATE effect size (i.e. effect size for compliers).
 #' @param sig.level     	significance level (Type I error probability). Default is 0.05.
 #' @param power         	power of test (1 minus Type II error probability).
 #' @param effect.size   	whether effect size (kappa) rather than absolute effect (tau) is used in power calculations. Default is \code{TRUE}.
-#' @param tau           	absolute effect. Must only be supplied if \code{effect.size = FALSE}.
+#' @param tau           	LATE absolute effect (i.e. absolute effect for compliers). Must only be supplied if \code{effect.size = FALSE}.
 #' @param omega             within-group standard deviation of the outcome. Must be supplied if \code{effect.size = FALSE}. See Details.
 #' @param assume.ord.means	whether ordered means assumption is made. Default is \code{FALSE}. See Details.
 #' @param r2dw				proportion of variation in D left unexplained by Z that is explained by W.
@@ -43,31 +43,59 @@
 #' Note also that the results along with additional information will be displayed in the console.
 #' @author Kirk Bansak and Eddie Yang
 #' @references Bansak, K. (2020). A Generalized Approach to Power Analysis for Local Average Treatment Effects. Statistical Science, 35(2), 254-271.
-#' @export
 #' @examples
+#' #EXAMPLE 1
+#' #Recovering power, without ordered-means assumption
+#' #powerLATE.cov(pi = 0.5, N = 3000, kappa = 0.25, r2dw = 0.15, r2yw = 0.10)
+#' results <- powerLATE.cov(pi = 0.5, N = 3000, kappa = 0.25, 
+#'                          r2dw = 0.15, r2yw = 0.10)
+#' results$input.parameter
+#' results$output.parameter
 #' 
-#' #######################################################
-#' ### Example 1: 
-#' ### LATE w/ equal assigment w/o ordered mean assumption
-#' #######################################################
+#' #EXAMPLE 2
+#' #Recovering power for various compliance rates,
+#' #without ordered-means assumption, and with unequal treatment-assignment probability
+#' powerLATE.cov(pZ = 0.25, pi = c(0.3,0.4,0.5,0.6,0.7), N = 3000, 
+#'               kappa = 0.25, r2dw = 0.15, r2yw = 0.10)
 #' 
-#' res <- powerLATE.cov(pZ=0.5, pi=0.35, kappa=seq(0.4, 1.0, 0.1), power=0.8,
-#'                      r2dw=0.2, r2yw=0.15)
+#' #EXAMPLE 3
+#' #Again recovering power for various compliance rates,
+#' #this time with the ordered-means assumption
+#' powerLATE.cov(pi = c(0.3,0.4,0.5,0.6,0.7), N = 3000, kappa = 0.25,
+#'               assume.ord.means = TRUE, r2dw = 0.15, r2yw = 0.10)
 #' 
-#' # also returns an invisible object: output.parameter
-#' res$output.parameter
+#' #EXAMPLE 4
+#' #Recovering power, without ordered-means assumption, 
+#' #this time using absolute effect rather than effect size
+#' powerLATE.cov(pi = 0.5, N = 3000, effect.size = FALSE, 
+#'               tau = 300, omega = 1500, r2dw = 0.15, r2yw = 0.10)
 #' 
-#' #######################################################
-#' ### Example 2: 
-#' ### LATE w/o equal assigment w/ ordered mean assumption
-#' #######################################################
+#' #EXAMPLE 5
+#' #Recovering required sample size for various compliance rates,
+#' #with ordered-means assumption
+#' powerLATE.cov(pi = c(0.5,0.6,0.7,0.8), kappa = 0.25, power = 0.8, 
+#'               assume.ord.means = TRUE, r2dw = 0.15, r2yw = 0.10)
 #' 
-#' res <- powerLATE.cov(pZ=0.67, pi=0.35, kappa=seq(0.4, 1.0, 0.1), 
-#'                 power=0.8, assume.ord.means=TRUE, r2dw=0.2, r2yw=0.15)
+#' #EXAMPLE 6
+#' #Recovering required sample size for various compliance rates,
+#' #with ordered-means assumption, and specifying absolute effect
+#' powerLATE.cov(pi = c(0.5,0.6,0.7,0.8), power = 0.8, 
+#'               assume.ord.means = TRUE, effect.size = FALSE, tau = 25, 
+#'               omega = 125, r2dw = 0.15, r2yw = 0.10)
 #' 
-#' # also returns an invisible object: output.parameter
-#' res$output.parameter
-
+#' #EXAMPLE 7
+#' #Recovering minimum detectable effect size for various sample sizes,
+#' #without ordered-means assumption
+#' powerLATE.cov(pi = 0.6, N = c(1000,1500,2000,2500,3000),
+#'               power = 0.8, r2dw = 0.15, r2yw = 0.10)
+#' 
+#' #EXAMPLE 8
+#' #Recovering minimum detectable effect (absolute) for various sample sizes,
+#' #with ordered-means assumption, and with unequal treatment-assignment probability
+#' powerLATE.cov(pZ = 0.4, pi = 0.6, N = c(1000,1500,2000,2500,3000),
+#'               power = 0.8, assume.ord.means = TRUE, 
+#'               effect.size = FALSE, omega = 50, r2dw = 0.15, r2yw = 0.10)
+#' @export
 
 powerLATE.cov <- function(
 	pZ = 0.5,
@@ -95,7 +123,7 @@ powerLATE.cov <- function(
 	if (!is.null(omega) && any(omega<=0)) stop("omega needs to be positive")
 	if (!missing(N) && any(N<=0)) stop("N needs to be positive")
 	if (!missing(kappa) && !is.null(tau)) stop("kappa and tau cannot be both supplied")
-	if (effect.size && !is.null(tau)) stop("tau is supplied instead of kappa while effect.size = FALSE. User should set effect.size = TRUE in order to work with tau in place of kappa")
+	if (effect.size && !is.null(tau)) stop("tau is supplied instead of kappa while effect.size = TRUE. User should set effect.size = FALSE in order to work with tau in place of kappa")
 	if (effect.size && !is.null(omega)) stop("omega is supplied while effect.size = TRUE.")
 	if (!effect.size && is.null(omega)) stop("omega must be supplied when effect.size=FALSE")
 	if (effect.size && sum(missing(kappa), missing(N), missing(power))!=1){
@@ -206,7 +234,6 @@ powerLATE.cov <- function(
 	cat(unlist(input))
 
 	multiple.input <- names(input[grepl(pattern = "= Multiple", x=unlist(input))])
-	message.target <- c("kappa (minimum detectable effect size)", "N (required sample size)", "Power")
 	output.name <- c(c("kappa", "N", "power")[target], paste0("User-inputted ", multiple.input))
 
 	if (length(out)==1 && any(out<0, !is.finite(out))){
@@ -221,13 +248,17 @@ powerLATE.cov <- function(
 		}		
 	}
 
+	message.target <- c("(upper) bound for kappa (minimum detectable effect size)", 
+		"(upper) bound for N (required sample size)", 
+		"(lower) bound for Power")
+
 	if (!effect.size && target==1){
 		out <- out*omega
 		output.name[1]  <- "tau"
-		message.target[1] <- "tau (minimum detectable effect)"
+		message.target[1] <- "(upper) bound for tau (minimum detectable effect)"
 	}
 
-	cat(paste0("Given these parameter values, the conservative bound for ", message.target[target], ":\n"))
+	cat(paste0("Given these parameter values, the conservative ", message.target[target], ":\n"))
 
 	if(length(multiple.input)!=0){
 		res <- structure(list(
